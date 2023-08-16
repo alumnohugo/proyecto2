d368f22c9345bbe392bdaa6fbb60396b90a61838
Hugo Morales <moralesbatzh@gmail.com>
2023-08-16 08:38:18 -0600

    se modifico la vista

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,25 +1,11 @@
-<div class="container mt-5">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-header">
-            Iniciar Sesión
-          </div>
-          <div class="card-body">
-            <form>
-              <div class="mb-3">
-                <label for="usuario" class="form-label">Usuario</label>
-                <input type="text" class="form-control" id="usuario" name="usuario" required>
-              </div>
-              <div class="mb-3">
-                <label for="contrasenia" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="contrasenia" name="contrasenia" required>
-              </div>
-              <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+<div class="row mb-3">
+  <div class="col text-center">
+    <h1>Ejemplo</h1>
   </div>
+</div>
+<div class="row justify-content-center">
+  <div class="col-lg-4">
+    <img src="./images/cit.png" width="100%" alt="">
+  </div>
+</div>
 <script src="build/js/inicio.js"></script>
